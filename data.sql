8c26ac207eee90585d47fe2cf6f61b27bea4bb41
Serg Skol <sps676@gmail.com>
2023-01-24 11:29:24 +0100

    Update and delete in transactions

--- data.sql
+++ data.sql
@@ -1,6 +1,15 @@
 /* Populate database with sample data. */
+-- Project 1
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', 'Feb 03, 2020', 0, TRUE, 10.23);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', 'Nov 15, 2018', 2, TRUE, 8.00);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', 'Jan 07, 2021', 1, FALSE, 15.04);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', 'May 12, 2017', 5, TRUE, 11.00);
 
-INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '03-02-2020', 0, TRUE, 10.23);
-INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '15-11-2018', 2, TRUE, 8.00);
-INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '07-01-2021', 1, FALSE, 15.04);
-INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '12-05-2017', 5, TRUE, 11.00);
+--Project 2
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', 'Feb 08, 2020', 0, FALSE, 11.00);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', 'Nov 15, 2021', 2, TRUE, 5.70);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', 'Apr 02, 1993', 3, FALSE, 12.13);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', 'Jun 12, 2005', 1, TRUE, 45.00);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', 'Jun 07, 2005', 7, TRUE, 20.40);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', 'Oct 13, 1998', 3, TRUE, 17.00);
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', 'May 14, 2022', 4, TRUE, 22.00);
